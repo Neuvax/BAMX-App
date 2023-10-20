19f3de4d50bb40c3b78fb657943aade07c5b0e2c
NolbertoCastro <nolber0703@gmail.com>
2023-10-20 08:10:36 -0600

    SCRUM-55 LogIn test finished

---
+++ test/cubit_test/auth_cubit_test.dart
@@ -0,0 +1,66 @@
+import "dart:async";
+
+import 'package:bloc_test/bloc_test.dart';
+import 'package:bamx_app/main.dart';
+import 'package:bamx_app/src/cubits/auth_cubit.dart';
+import 'package:bamx_app/src/repository/auth_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAuthRepo extends Mock implements AuthRepository {}
+
+void main() {
+  late MockAuthRepo mockRepo;
+
+  setUp(() async {
+    await getIt.reset();
+    mockRepo = MockAuthRepo();
+    getIt.registerSingleton<AuthRepository>(mockRepo);
+  });
+
+  blocTest<AuthCubit, CurrentAuthState>(
+    'Signed out state will be emitted',
+    build: () {
+      when(() => mockRepo.onAuthStateChanged)
+          .thenAnswer((_) => Stream.fromIterable([null]));
+      return AuthCubit();
+    },
+    act: (cubit) async => await cubit.init(),
+    expect: () => [
+      CurrentAuthState.signedOut,
+    ],
+  );
+
+  blocTest<AuthCubit, CurrentAuthState>(
+    'Signed in state will be emitted',
+    build: () {
+      when(() => mockRepo.onAuthStateChanged)
+          .thenAnswer((_) => Stream.fromIterable(['someUserID']));
+      return AuthCubit();
+    },
+    act: (cubit) => cubit.init(),
+    expect: () => [
+      CurrentAuthState.signedIn,
+    ],
+  );
+
+  blocTest<AuthCubit, CurrentAuthState>(
+      'Signed out state will be emitted after calling signOut',
+      build: () {
+        when(() => mockRepo.onAuthStateChanged)
+            .thenAnswer((_) => Stream.fromIterable(['someUserID']));
+        when(() => mockRepo.signOut()).thenAnswer((_) async {});
+        return AuthCubit();
+      },
+      act: (cubit) async {
+        await cubit.init();
+        await cubit.signOut();
+      },
+      expect: () => [
+            CurrentAuthState.signedIn,
+            CurrentAuthState.signedOut,
+          ],
+      verify: (cubit) {
+        verify(() => mockRepo.signOut()).called(1);
+      });
+}
