3eb38275477a7d8a7c9057a5478fa99c3663bd79
Armando Terrazas Gómez <A01640924@tec.mx>
2023-10-28 13:14:45 -0600

    SCRUM-64 Add FirebaseDataSource class for donation

---
+++ lib/src/data-source/firebase_data_source.dart
@@ -0,0 +1,24 @@
+import 'package:bamx_app/src/model/item_donacion.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseDataSource {
+  User get currentUser {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      throw Exception('User not logged in');
+    }
+    return user;
+  }
+
+  FirebaseFirestore get firestore => FirebaseFirestore.instance;
+
+  ///Get all documents from the collection "items"
+  Stream<Iterable<ItemDonacion>> getItems() {
+    return firestore.collection('items').snapshots().map((snapshot) {
+      return snapshot.docs
+          .map((doc) => ItemDonacion.fromMap(doc.id, doc.data()))
+          .toList(); // Convertir a lista
+    });
+  }
+}
