0aa5c275d03d57409f392ab67494197752cf50d4
Armando Terrazas Gómez <A01640924@tec.mx>
2023-10-30 07:46:04 -0600

    SCRUM-64 Added badge to cart icon in appbar

--- lib/src/components/app_bar.dart
+++ lib/src/components/app_bar.dart
@@ -1,6 +1,8 @@
+import 'package:bamx_app/src/cubits/cart_cubit.dart';
 import 'package:bamx_app/src/routes/routes.dart';
 import 'package:bamx_app/src/utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   const MyAppBar({Key? key}) : super(key: key);
@@ -21,34 +23,67 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-          border: Border(
-              bottom: BorderSide(
-        color: MyColors.accent,
-        width: 0.2,
-      ))),
-      child: AppBar(
-        title: Image.asset(
-          "assets/images/bamx_logo.png",
-          fit: BoxFit.cover,
-          height: 40,
-        ),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.shopping_cart_outlined),
-            onPressed: () => navigateToRoute(context, Routes.cart),
+    return BlocProvider(
+        create: (context) => CartCubit()..init(),
+        child: Container(
+          decoration: const BoxDecoration(
+              border: Border(
+                  bottom: BorderSide(
             color: MyColors.accent,
-            iconSize: 27.0,
+            width: 0.2,
+          ))),
+          child: AppBar(
+            title: Image.asset(
+              "assets/images/bamx_logo.png",
+              fit: BoxFit.cover,
+              height: 40,
+            ),
+            actions: [
+              BlocBuilder<CartCubit, CartState>(
+                builder: (context, state) {
+                  return Stack(
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.shopping_cart_outlined),
+                        onPressed: () => navigateToRoute(context, Routes.cart),
+                        color: MyColors.accent,
+                        iconSize: 27.0,
+                      ),
+                      if (state.cartItems.isNotEmpty && !state.isLoading)
+                        Positioned(
+                          right: 0,
+                          child: Container(
+                            padding: const EdgeInsets.all(1),
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                            constraints: const BoxConstraints(
+                              minWidth: 12,
+                              minHeight: 12,
+                            ),
+                            child: Text(
+                              '${state.cartItems.length}',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 8,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                    ],
+                  );
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.person),
+                onPressed: () => navigateToRoute(context, Routes.userProfile),
+                color: MyColors.accent,
+                iconSize: 27.0,
+              )
+            ],
           ),
-          IconButton(
-            icon: const Icon(Icons.person),
-            onPressed: () => navigateToRoute(context, Routes.userProfile),
-            color: MyColors.accent,
-            iconSize: 27.0,
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
