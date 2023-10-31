030c230043741b1c9487a998668fe1809bf3d366
PAPIPHOX <jpenariestra@gmail.com>
2023-10-31 08:12:42 -0600

    inicio de sesion google

--- lib/src/pages/sign_in.dart
+++ lib/src/pages/sign_in.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({Key? key}) : super(key: key);
@@ -13,6 +14,7 @@ class SignInPage extends StatelessWidget {
       body: SignInScreen(
         providers: [
           EmailAuthProvider(),
+          GoogleProvider(clientId: "773494367421-oggkhjsdg0b29fgluid0ammb2hnr7tfe.apps.googleusercontent.com"),
         ],
       )
     );
