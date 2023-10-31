4cdad5d4aa49607385850af8edbd6505ca26cfba
Armando Terrazas Gómez <A01640924@tec.mx>
2023-10-30 21:06:28 -0600

    SCRUM-63 Refactored to include to lists in one page

--- lib/src/components/donations_list.dart
+++ lib/src/components/donations_list.dart
@@ -1,71 +1,55 @@
 import 'package:bamx_app/src/cubits/donaciones_cubit.dart';
+import 'package:bamx_app/src/model/item_donacion.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class DonationsList extends StatelessWidget {
-  final ListaDonacionesState state;
-  final String title;
+class DonationList extends StatelessWidget {
+  final List<ItemDonacion> donations;
 
-  const DonationsList({Key? key, required this.title, required this.state})
-      : super(key: key);
+  const DonationList({super.key, required this.donations});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Align(
-          alignment: Alignment.centerLeft,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              title,
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          final itemDonacion = donations.elementAt(index);
+          return Card(
+            margin: const EdgeInsets.only(bottom: 10),
+            child: ListTile(
+              leading: Image.network(itemDonacion.imagen),
+              title: Text(itemDonacion.nombre),
+              subtitle: Text("Unidad: ${itemDonacion.unidad}"),
+              trailing: IconButton(
+                onPressed: () {
+                  BlocProvider.of<ListaDonacionesCubit>(context)
+                      .addItemToCart(itemDonacion);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Row(
+                        children: <Widget>[
+                          const Icon(Icons.check, color: Colors.white),
+                          const SizedBox(width: 20),
+                          Expanded(
+                            child: Text(
+                                '${itemDonacion.nombre} agregado al carrito',
+                                style: const TextStyle(color: Colors.white)),
+                          ),
+                        ],
+                      ),
+                      duration: const Duration(seconds: 2),
+                      backgroundColor: Colors.green,
+                    ),
+                  );
+                },
+                icon: const Icon(Icons.add_shopping_cart),
+                color: Colors.green,
               ),
             ),
-          ),
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: state.listaItemsDonaciones.length,
-            itemBuilder: (context, index) {
-              final itemDonacion = state.listaItemsDonaciones.elementAt(index);
-              return Card(
-                margin: const EdgeInsets.only(bottom: 10),
-                child: ListTile(
-                  leading: Image.network(itemDonacion.imagen),
-                  title: Text(itemDonacion.nombre),
-                  subtitle: Text("Unidad: ${itemDonacion.unidad}"),
-                  trailing: IconButton(
-                    onPressed: () {
-                      BlocProvider.of<ListaDonacionesCubit>(context)
-                          .addItemToCart(itemDonacion);
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Row(
-                            children: <Widget>[
-                              const Icon(Icons.check, color: Colors.white),
-                              const SizedBox(width: 20),
-                              Expanded(
-                                child: Text('${itemDonacion.nombre} agregado al carrito', style: const TextStyle(color: Colors.white)),
-                              ),
-                            ],
-                          ),
-                          duration: const Duration(seconds: 2),
-                          backgroundColor: Colors.green,
-                        ),
-                      );
-                    },
-                    icon: const Icon(Icons.add_shopping_cart),
-                    color: Colors.green,
-                  ),
-                ),
-              );
-            },
-          ),
-        ),
-      ],
+          );
+        },
+        childCount: donations.length,
+      ),
     );
   }
 }
--- lib/src/pages/donations_page.dart
+++ lib/src/pages/donations_page.dart
@@ -9,29 +9,71 @@ class DonationsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => ListaDonacionesCubit()..init(),
-      child: BlocBuilder<ListaDonacionesCubit, ListaDonacionesState>(
-        builder: (context, state) {
-          //If the state is loading, show a progress indicator
-          if (state.isLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          //If there are no items, show a message
-          else if (state.listaItemsDonaciones.isEmpty) {
-            return const Center(
-              child: Text("No hay donaciones disponibles"),
-            );
-          } else {
-            return Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: DonationsList(title: "Artículos para donación",state: state),
-            );
-          }
-        },
-      )
-    );
+        create: (context) => ListaDonacionesCubit()..init(),
+        child: BlocBuilder<ListaDonacionesCubit, ListaDonacionesState>(
+          builder: (context, state) {
+            //If the state is loading, show a progress indicator
+            if (state.isLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            //If there are no items, show a message
+            else if (state.listaItemsDonaciones.isEmpty) {
+              return const Center(
+                child: Text("No hay donaciones disponibles"),
+              );
+            } else {
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: CustomScrollView(
+                  slivers: <Widget>[
+                    const SliverToBoxAdapter(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                "Donaciones prioritarias",
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              "Estas son las donaciones que más se necesitan en este momento ¡Obtén dobles puntos al donar estos artículos!",
+                              style: TextStyle(
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    DonationList(
+                        donations: state.listaItemsDonaciones.toList()),
+                    const SliverToBoxAdapter(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          "Otros artículos para donar",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    DonationList(
+                        donations: state.listaItemsDonaciones.toList()),
+                  ],
+                ),
+              );
+            }
+          },
+        ));
   }
 }
-
