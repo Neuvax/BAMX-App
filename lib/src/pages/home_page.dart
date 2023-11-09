ba681caf5d0ebf91204ebc541b6a855f0ba357fa
Armando Terrazas Gómez <A01640924@tec.mx>
2023-11-08 18:43:24 -0600

    Add home page layout with welcome message, notices, pending donations, and recent recognitions

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -5,8 +5,120 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home Page'),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 28),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "¡Bienvenido a BAMX App!",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            const SizedBox(height: 8),
+            const Text(
+              "Esta aplicación te permite donar al Bancos de Alimentos de Jalisco de una manera fácil y segura.",
+              style: TextStyle(
+                fontSize: 14,
+              ),
+            ),
+            const SizedBox(height: 28),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Avisos",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            const SizedBox(height: 28),
+            //Horizontal List,
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 3,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    margin: const EdgeInsets.only(right: 10),
+                    child: Container(
+                      width: 200,
+                      height: 200,
+                      color: Colors.grey[300],
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: 28),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Donaciones pendientes",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            const SizedBox(height: 28),
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 3,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    margin: const EdgeInsets.only(right: 10),
+                    child: Container(
+                      width: 200,
+                      height: 200,
+                      color: Colors.grey[300],
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: 28),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Últimos Reconocimientos",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            const SizedBox(height: 28),
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 3,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    margin: const EdgeInsets.only(right: 10),
+                    child: Container(
+                      width: 200,
+                      height: 200,
+                      color: Colors.grey[300],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
