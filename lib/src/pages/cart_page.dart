b986ad2a6782be55ed8651aaa36a4054a5205f5b
PAPIPHOX <jpenariestra@gmail.com>
2023-11-13 15:43:50 -0600

    Refactor cart page UI to display submit button and fix minor bugs

--- lib/src/pages/cart_page.dart
+++ lib/src/pages/cart_page.dart
@@ -11,40 +11,57 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: const MyAppBar(),
-        body: BlocProvider(
-            create: (context) => CartCubit()..init(),
-            child: BlocBuilder<CartCubit, CartState>(
-              builder: (context, state) {
-                //If the state is loading, show a progress indicator
-                if (state.isLoading) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                //If there are no items, show a message
-                else if (state.cartItems.isEmpty) {
-                  return const Center(
-                    child: Text("No hay donaciones disponibles"),
-                  );
-                } else {
-                  final cartItemsList = state.cartItems.toList();
-                  return Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: ListView.builder(
-                      itemCount: cartItemsList.length,
-                      itemBuilder: (context, index) {
-                        return ListItem(
-                          item: cartItemsList[index].item,
-                          quantity:
-                              ValueNotifier<int>(cartItemsList[index].cantidad),
-                        );
-                      },
+      appBar: const MyAppBar(),
+      body: BlocProvider(
+        create: (context) => CartCubit()..init(),
+        child: BlocBuilder<CartCubit, CartState>(
+          builder: (context, state) {
+            // If the state is loading, show a progress indicator
+            if (state.isLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            // If there are no items, show a message
+            else if (state.cartItems.isEmpty) {
+              return const Center(
+                child: Text("No hay donaciones disponibles"),
+              );
+            } else {
+              final cartItemsList = state.cartItems.toList();
+              return Column(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: ListView.builder(
+                        itemCount: cartItemsList.length,
+                        itemBuilder: (context, index) {
+                          return ListItem(
+                            item: cartItemsList[index].item,
+                            quantity:
+                                ValueNotifier<int>(cartItemsList[index].cantidad),
+                          );
+                        },
+                      ),
                     ),
-                  );
-                }
-              },
-            )));
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 16.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // TODO: Add logic for submitting the cart
+                      },
+                      child: const Text('Submit'),
+                    ),
+                  ),
+                ],
+              );
+            }
+          },
+        ),
+      ),
+    );
   }
 }
 
@@ -70,13 +87,12 @@ class _ListItemState extends State<ListItem> {
           Row(
             children: [
               RawMaterialButton(
-                onPressed: (){
+                onPressed: () {
                   context.read<CartCubit>().deleteItemToCart(widget.item);
                 },
-                shape: CircleBorder(),
+                shape: const CircleBorder(),
                 fillColor: MyColors.primary,
                 child: const Icon(Icons.remove, color: Colors.white),
-
               ),
               ValueListenableBuilder<int>(
                 valueListenable: widget.quantity,
@@ -84,15 +100,13 @@ class _ListItemState extends State<ListItem> {
                   return Text(value.toString());
                 },
               ),
-
               RawMaterialButton(
-                onPressed: (){
+                onPressed: () {
                   context.read<CartCubit>().addItemToCart(widget.item);
                 },
-                shape: CircleBorder(),
+                shape: const CircleBorder(),
                 fillColor: MyColors.yellow,
                 child: const Icon(Icons.add, color: Colors.white),
-
               ),
               IconButton(
                 onPressed: () {
