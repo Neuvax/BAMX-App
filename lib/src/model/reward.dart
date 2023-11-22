30903792adb3421348acafed3bb4db51db0057de
NolbertoCastro <nolber0703@gmail.com>
2023-11-22 17:42:56 -0600

    SCRUM-81 rewards view finished, title added

--- lib/src/model/reward.dart
+++ lib/src/model/reward.dart
@@ -3,16 +3,18 @@ import 'package:equatable/equatable.dart';
 class Reward extends Equatable {
   final String imagen;
   final String nombre;
+  final String rewardDate;
 
-  const Reward({required this.imagen, required this.nombre});
+  const Reward(
+      {required this.imagen, required this.nombre, required this.rewardDate});
 
   @override
-  List<Object> get props => [imagen, nombre];
+  List<Object> get props => [imagen, nombre, rewardDate];
 
   factory Reward.fromMap(Map<String, dynamic> data) {
     return Reward(
-      imagen: data['image'],
-      nombre: data['rewardName'],
-    );
+        imagen: data['image'],
+        nombre: data['rewardName'],
+        rewardDate: data['rewardDate']);
   }
 }
--- lib/src/pages/rewards_page.dart
+++ lib/src/pages/rewards_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:bamx_app/src/utils/colors.dart';
 import 'package:bamx_app/src/cubits/reward_cubit.dart';
 
 class RewardsPage extends StatelessWidget {
@@ -14,46 +13,63 @@ class RewardsPage extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: BlocBuilder<RewardCubit, RewardState>(
           builder: (context, state) {
-            if (!state.isLoading) {
-              if (state.rewards.isNotEmpty) {
-                return ListView.builder(
-                  itemCount: state.rewards.length,
-                  itemBuilder: (context, index) {
-                    final reward = state.rewards.elementAt(index);
-                    return Card(
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 4),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 8.0), // Padding arriba y abajo
-                        child: ListTile(
-                          leading: Image.network(reward.imagen),
-                          title: Text(
-                            reward.nombre,
-                            style: const TextStyle(fontSize: 18),
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                );
-              } else {
-                return const Center(
-                  child: Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Text(
-                      "No hay recompensas disponibles",
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.grey,
-                      ),
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Mis Recompensas",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
-                );
-              }
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
+                ),
+                Expanded(
+                  child: state.isLoading
+                      ? const Center(child: CircularProgressIndicator())
+                      : state.rewards.isNotEmpty
+                          ? ListView.builder(
+                              itemCount: state.rewards.length,
+                              itemBuilder: (context, index) {
+                                final reward = state.rewards.elementAt(index);
+                                return Card(
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 8, vertical: 4),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 8.0),
+                                    child: ListTile(
+                                      leading: Image.network(reward.imagen),
+                                      title: Text(
+                                        reward.nombre,
+                                        style: const TextStyle(fontSize: 18),
+                                      ),
+                                      subtitle: Text(
+                                        reward.rewardDate,
+                                        style: const TextStyle(fontSize: 16),
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              },
+                            )
+                          : const Center(
+                              child: Padding(
+                                padding: EdgeInsets.all(20.0),
+                                child: Text(
+                                  "No hay recompensas disponibles",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ),
+                            ),
+                ),
+              ],
+            );
           },
         ),
       ),
