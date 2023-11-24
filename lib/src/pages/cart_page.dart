22910a7bad6c705cbdefb51fd383f0a3ba32bf89
Neuvax <a01640826@tec.mx>
2023-11-23 18:11:48 -0600

    Update zoom level and marker info windows in CartPage

--- lib/src/pages/cart_page.dart
+++ lib/src/pages/cart_page.dart
@@ -11,35 +11,39 @@ class CartPage extends StatelessWidget {
 
   static const CameraPosition _cameraPosition = CameraPosition(
     target: LatLng(20.6597, -103.3496),
-    zoom: 12,
+    zoom: 11,
   );
   static final Set<Marker> _markers = {
     const Marker(
       markerId: MarkerId('marker_1'),
       position: LatLng(20.634833787881437, -103.41699354338266),
       infoWindow: InfoWindow(
-          title: 'Parroquia de San Jeronimo',
+          title:
+              'Parroquia de San Jeronimo\n\n Hora de atención: 8:00 Am - 5:00 Pm',
           snippet: 'C. Sagitario 4170, La Calma, 45070 Zapopan, Jal.'),
     ),
     const Marker(
       markerId: MarkerId('marker_2'),
       position: LatLng(20.687425822741695, -103.40913095757375),
       infoWindow: InfoWindow(
-          title: 'Nuestra Señora de la Salud',
+          title:
+              'Nuestra Señora de la Salud\n\n Hora de atención: 8:00 Am - 5:00 Pm',
           snippet: 'P.º Lomas Altas 265, Lomas del Valle, 45128 Zapopan, Jal.'),
     ),
     const Marker(
       markerId: MarkerId('marker_3'),
       position: LatLng(20.665764819223188, -103.40659032873587),
       infoWindow: InfoWindow(
-          title: 'Parroquia Santa Rita de Casia',
+          title:
+              'Parroquia Santa Rita de Casia\n\n Hora de atención: 8:00 Am - 5:00 Pm',
           snippet: 'Av Guadalupe 1668, Chapalita Oriente, 45040 Zapopan, Jal.'),
     ),
     const Marker(
       markerId: MarkerId('marker_4'),
       position: LatLng(20.68286052206871, -103.41239039951039),
       infoWindow: InfoWindow(
-          title: 'Parroquia de San Nicolas de Bari',
+          title:
+              'Parroquia de San Nicolas de Bari\n\n Hora de atención: 8:00 Am - 5:00 Pm',
           snippet:
               'Cáncer # 4190-A Col 45120, Juan Manuel Vallarta, 45120 Zapopan, Jal.'),
     ),
@@ -47,7 +51,8 @@ class CartPage extends StatelessWidget {
       markerId: MarkerId('marker_5'),
       position: LatLng(20.74084831273084, -103.41075167017452),
       infoWindow: InfoWindow(
-          title: 'Nuestra Señora de la caridad del Cobre',
+          title:
+              'Nuestra Señora de la caridad del Cobre\n\n Hora de atención: 8:00 Am - 5:00 Pm',
           snippet:
               'C. Arco Decio 1202, Arcos de Zapopan, 45130 Guadalajara, Jal.'),
     ),
@@ -55,7 +60,8 @@ class CartPage extends StatelessWidget {
       markerId: MarkerId('marker_6'),
       position: LatLng(20.651622634002397, -103.39928772511166),
       infoWindow: InfoWindow(
-          title: 'Parroquia de la Resurrección del Señor',
+          title:
+              'Parroquia de la Resurrección del Señor\n\n Hora de atención: 8:00 Am - 5:00 Pm',
           snippet:
               'Av Plaza del Sol 253, Rinconada del Sol, 45050 Zapopan, Jal.'),
     ),
@@ -63,7 +69,8 @@ class CartPage extends StatelessWidget {
       markerId: MarkerId('marker_7'),
       position: LatLng(20.68462294506873, -103.37692883191681),
       infoWindow: InfoWindow(
-          title: 'Parroquia de la Santa Cruz',
+          title:
+              'Parroquia de la Santa Cruz\n\n Hora de atención: 8:00 Am - 5:00 Pm',
           snippet:
               'Av. Manuel Acuña 2380, Ladrón de Guevara, Ladron De Guevara, 44650 Guadalajara, Jal.'),
     ),
@@ -71,7 +78,7 @@ class CartPage extends StatelessWidget {
       markerId: MarkerId('marker_8'),
       position: LatLng(20.649229680193113, -103.40268122873586),
       infoWindow: InfoWindow(
-          title: 'Plaza del Sol',
+          title: 'Plaza del Sol\n\n Hora de atención: 8:00 Am - 5:00 Pm',
           snippet:
               'Av. Adolfo López Mateos Sur 2375, Cd del Sol, 45050 Zapopan, Jal.'),
     ),
@@ -79,7 +86,7 @@ class CartPage extends StatelessWidget {
       markerId: MarkerId('marker_9'),
       position: LatLng(20.71042634669587, -103.41198375757429),
       infoWindow: InfoWindow(
-          title: 'Plaza Andares',
+          title: 'Plaza Andares\n\n Hora de atención: 8:00 Am - 5:00 Pm',
           snippet:
               'Blvrd Puerta de Hierro 4965, Puerta de Hierro, 44100 Guadalajara, Jal.'),
     ),
@@ -87,7 +94,8 @@ class CartPage extends StatelessWidget {
       markerId: MarkerId('marker_10'),
       position: LatLng(20.6736284810099, -103.40496814223849),
       infoWindow: InfoWindow(
-          title: 'La Gran Plaza Fashion Mall',
+          title:
+              'La Gran Plaza Fashion Mall\n\n Hora de atención: 8:00 Am - 5:00 Pm',
           snippet:
               'Av. Ignacio L Vallarta 3959, Don Bosco Vallarta, 45049 Zapopan, Jal.'),
     ),
@@ -117,7 +125,7 @@ class CartPage extends StatelessWidget {
                   const Padding(
                     padding: EdgeInsets.all(16.0),
                     child: Text(
-                      "Ubicación de los Centros de Acopio",
+                      "Mi Carrito",
                       style:
                           TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                       textAlign: TextAlign.center,
@@ -145,6 +153,15 @@ class CartPage extends StatelessWidget {
                       child: const Text('Confirmar'),
                     ),
                   ),
+                  const Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      "Centros de Acopio",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
                   Padding(
                     padding: const EdgeInsets.all(32.0),
                     child: Container(
@@ -161,23 +178,6 @@ class CartPage extends StatelessWidget {
                           zoomControlsEnabled: true,
                         ),
                       ),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Text(
-                      'Horarios de atención',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Text(
-                      '8:00 Am - 5:00 Pm',
-                      style: TextStyle(fontSize: 16),
-                      textAlign: TextAlign.center,
                     ),
                   ),
                 ],
